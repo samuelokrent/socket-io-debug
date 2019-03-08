4abcd37d69fcdd69107b56ea1ac593533d8e7744
Sam Okrent <samuelokrent@gmail.com>
2019-03-08 17:47:58 -0600

    Add report statements

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,4 +1,4 @@
 DIR=$(dirname $(realpath $0))
 rm -rf ~/bigmarker.webrtc/node_modules/socket.io
-cd .. && cp -r $DIR ~/bigmarker.webrtc/node_modules/socket.io
+cp -r $DIR ~/bigmarker.webrtc/node_modules/socket.io
 cd ~/bigmarker.webrtc/node_modules/socket.io && npm install
---
+++ UNINSTALL.sh
@@ -0,0 +1,2 @@
+rm -rf ~/bigmarker.webrtc/node_modules/socket.io
+cd ~/bigmarker.webrtc && npm install
